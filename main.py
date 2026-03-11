59663737287403b95210b67af48f67fba2209313
Vineet Gaira <vineetgaira1729@gmail.com>
2026-03-11 16:52:14 +0530

    Added move file feature

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import os
 import shutil 
 import pathlib
 
+FILE_CATEGORIES = {
+    "Images": ["jpg", "png", "jpeg", "gif"],
+    "Videos": ["mp4", "mkv", "avi"],
+    "Documents": ["pdf", "docx", "txt"],
+    "Archives": ["zip", "rar"]
+}
+
 folder_path=input("Enter the folder path: ")
+
+    
 
 if not os.path.exists(folder_path):
     print("Folder does not exist.")
@@ -11,17 +20,47 @@ else:
 
     files=os.listdir(folder_path)
 
-    for file in files:
+    for item in files:
 
-        full_path=os.path.join(folder_path, file)
+        full_path=os.path.join(folder_path, item)
+        
+        if not os.path.exists(full_path):   
+            continue
+
 
         if os.path.isfile(full_path):
             
-            extension=file.split(".")[-1]
-        
-            print(f"Filename : {file} | Extension : {extension}")
+            extension=item.split(".")[-1].lower()
             
-           
+            catogory_found=False
+
+            for category, extensions in FILE_CATEGORIES.items():
+
+                if extension in extensions:
+
+                    catogory_folder=os.path.join(folder_path,category)
+
+                    os.makedirs(catogory_folder,exist_ok=True)
+
+                    shutil.move(full_path, os.path.join(catogory_folder,item)) 
+
+                    print(f"Moved {item} -> {category}")   
+
+                    catogory_found=True    
+
+                    break   
+
+            if not catogory_found:
+
+                other_folder=os.path.join(folder_path,"Others")
+
+                os.makedirs(other_folder, exist_ok=True)
+
+                shutil.move(full_path, os.path.join(other_folder,item))
+
+                print(f"Moved {item} -> Others")
+
+
 
 
 
